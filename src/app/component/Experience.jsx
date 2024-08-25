3e1cad5684fa5307ecd9ce334005a068fe7ed1eb
Niketdeshmukh <niketdeshmukh2002@gmail.com>
2024-08-25 11:06:25 +0530

    updated experience section

--- src/app/component/Experience.jsx
+++ src/app/component/Experience.jsx
@@ -1,8 +1,8 @@
 "use client";
-import React, { useRef, useEffect } from 'react';
-import '../styles/Experience.css';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import React, { useRef, useEffect } from "react";
+import "../styles/Experience.css";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -42,69 +42,107 @@ const Experience = () => {
 
   return (
     <section ref={container} id="experience-section" className="gap">
-      <h2 className="numbered__heading_02"><span>My Experience</span></h2>
+      <h2 className="numbered__heading_02">
+        <span>My Experience</span>
+      </h2>
       <div className="overlay-wrapper">
         <div className="fade-in">
           <div className="experience-wrapper">
             <div id="timeline">
               <div className="timeline-block">
                 <div className="timeline-content">
-                  <h2 className="position">HCI Research Assistant</h2>
-                  <h3 className="company"><a href="http://www.ccis.northeastern.edu/" target="_blank">NU College of Computer
-                      & Information Science</a></h3>
-                  <p className="job-desc">Assisted a computer science PhD student in the development of a software prototype
-                    built with HTML, CSS, JavaScript, and jQuery to facilitate better understanding of parent-child
-                    communication preferences in low-SES (Socio-Economic Status) families. Developed remotely to meet
-                    weekly deadlines and project checkpoints.</p>
+                  <h2 className="position">Hostingspell | Grownix Ventures </h2>
+                  <h3 className="company">
+                    <a href="https://hostingspell.com/" target="_blank">
+                      Internship
+                    </a>
+                  </h3>
+                  <p className="job-desc">
+                    I worked as a developer where I was responsible for
+                    integrating APIs, developing and customizing WordPress
+                    sites, and rigorously testing APIs to ensure seamless
+                    functionality. I collaborated with the team remotely,
+                    consistently meeting weekly deadlines and successfully
+                    hitting key project checkpoints, which honed my time
+                    management and problem-solving skills in a real-world
+                    setting.
+                  </p>
                 </div>
                 <div className="timeline-img">
                   <div className="sprite research"></div>
-                </div> <span className="date">Mar - May 2015</span>
+                </div>{" "}
+                <span className="date">May - Jul 2022</span>
               </div>
               <div className="timeline-block">
                 <div className="timeline-content">
-                  <h2 className="position">HCI Teaching Assistant</h2>
-                  <h3 className="company"><a href="http://www.ccis.northeastern.edu/" target="_blank">NU College of Computer
-                      & Information Science</a></h3>
-                  <p className="job-desc">Assisted professor with grading homework and project assignments. Held office hours
-                    to aid students in their understanding of core HCI concepts and reported to the professor the extent
-                    of their understanding.</p>
+                  <h2 className="position">CRTD Technologies</h2>
+                  <h3 className="company">
+                    <a href="http://www.crtd.in/" target="_blank">
+                      Internship
+                    </a>
+                  </h3>
+                  <p className="job-desc">
+                    Spearheaded the development of a dynamic and user-friendly
+                    website at CRTD, utilizing JavaScript to create an engaging
+                    online platform.
+                    <br />
+                    Leveraged strong front-end development skills to design and
+                    implement interactive features, ensuring a seamless and
+                    visually appealing user experience.
+                  </p>
                 </div>
                 <div className="timeline-img">
                   <div className="sprite academic"></div>
-                </div> <span className="date">Jan - May 2016</span>
+                </div>{" "}
+                <span className="date">Oct - Nov 2022</span>
               </div>
               <div className="timeline-block">
                 <div className="timeline-content">
-                  <h2 className="position">President</h2>
-                  <h3 className="company"><a href="http://nuwit.ccs.neu.edu" target="_blank">NU Women in Tech</a></h3>
-                  <p className="job-desc">Lead weekly club meetings to foster a community of diverse women in CS. Manage a
-                    variety of academic and social activities throughout the semester, including tech talks, student
-                    panels, and workshops. Communicate regularly with e-board and administrative organizations.</p>
+                  <h2 className="position">SFR Medical</h2>
+                  <h3 className="company">
+                    <a href="https://sfrmedical.com/" target="_blank">
+                      Internship
+                    </a>
+                  </h3>
+                  <p className="job-desc">
+                  Implemented security best practices and compliance standards
+                    for GCP VM environments, enhancing data protection, also
+                    implemented comprehensive monitoring solutions.
+                    <br /> Conducted comprehensive testing, including functional
+                    and user acceptance testing, to guarantee the robustness and
+                    reliability of the web portal.
+                  </p>
                 </div>
                 <div className="timeline-img">
                   <div className="sprite university"></div>
-                </div> <span className="date">May 2016 - Present</span>
+                </div>{" "}
+                <span className="date">Aug - Oct 2023</span>
               </div>
               <div className="timeline-block">
                 <div className="timeline-content">
-                  <h2 className="position">Developer</h2>
-                  <h3 className="company"><a href="http://www.northeastern.edu/scout/" target="_blank">Scout Studio</a></h3>
-                  <p className="job-desc">Collaborated with a small team of student designers to spearhead a new design system
-                    for Scout’s first annual student-led design conference at Northeastern. Worked closely with designers
-                    and management team to develop, document, and manage the conference’s marketing website using Jekyll,
-                    Sass, and JavaScript.</p>
+                  <h2 className="position">Blue Stock</h2>
+                  <h3 className="company">
+                    <a href="https://bluestock.in/" target="_blank">
+                      SDE Intern
+                    </a>
+                  </h3>
+                  <p className="job-desc">
+                  During my SDE internship at Bluestock, I contributed to developing scalable web applications, focusing on optimizing backend processes and enhancing user experience. 
+                  <br />I collaborated closely with the engineering team, working on API development, debugging, and performance optimization. My work led to improved application efficiency and a smoother user interface.
+                  </p>
                 </div>
                 <div className="timeline-img">
                   <div className="sprite university"></div>
-                </div> <span className="date">Jan 2017 - Present</span>
+                </div>{" "}
+                <span className="date">Aug - Sep 2024</span>
               </div>
-            </div> <i className="down-arrow"></i>
+            </div>{" "}
+            <i className="down-arrow"></i>
           </div>
         </div>
       </div>
     </section>
   );
-}
+};
 
 export default Experience;
